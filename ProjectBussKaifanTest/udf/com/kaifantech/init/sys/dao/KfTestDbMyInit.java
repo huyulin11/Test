18959518e86a57a173c78a81f37b2bf3d1e27d67
ahacs <583937325@qq.com>
2020-05-19 17:33:46 +0800

    ss

--- ProjectBussKaifanTest/udf/com/kaifantech/init/sys/dao/KfTestDbMyInit.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/init/sys/dao/KfTestDbMyInit.java
@@ -3,8 +3,6 @@ package com.kaifantech.init.sys.dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.kaifantech.init.sys.params.AppConfKeys;
-
 public class KfTestDbMyInit extends BaseDao {
 	protected static void createDB(List<String> sqls) {
 		sqls.add("CREATE DATABASE `" + UdfDbs.OP_DB + "`");
@@ -15,14 +13,12 @@ public class KfTestDbMyInit extends BaseDao {
 	}
 
 	protected static void truncate(List<String> sqls) {
-		sqls.add("truncate table " + UdfDbs.CONF_DB + "." + AppConfKeys.WMS_ALLOC_LOCK);
 		sqls.add("truncate table " + AppTables.WMS_RECEIPT_REQUEST_DETAIL);
 		sqls.add("truncate table " + AppTables.WMS_RECEIPT_REQUEST_MAIN);
 		sqls.add("truncate table " + AppTables.WMS_SHIPMENT_REQUEST_DETAIL);
 		sqls.add("truncate table " + AppTables.WMS_SHIPMENT_REQUEST_MAIN);
 		sqls.add("truncate table " + AppTables.WMS_TRANSFER_REQUEST_DETAIL);
 		sqls.add("truncate table " + AppTables.WMS_TRANSFER_REQUEST_MAIN);
-		sqls.add("truncate table " + UdfDbs.CONF_DB + "." + AppConfKeys.COMBINED_TU_INFO);
 		sqls.add("truncate table " + AppTables.ALLOC_TXM_INFO);
 		sqls.add("update " + AppTables.ALLOCATION_ITEM_INFO + " set num=0,status=1 where 1=1 ");
 	}
