f6e555d1e11ff2ff90e7b03c456078fc43dc0259
ahacs <583937325@qq.com>
2020-05-19 14:23:20 +0800

    ss

--- ProjectBussKaifanTest/udf/com/kaifantech/component/timer/connect/client/KfTestIotConnectAsClientTimer.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/timer/connect/client/KfTestIotConnectAsClientTimer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import com.kaifantech.component.comm.worker.client.IConnectWorker;
 import com.kaifantech.init.sys.UdfBusinessInfo;
-import com.kaifantech.init.sys.BaseBusinessInfo;
+import com.kaifantech.init.sys.AppBusinessInfo;
 import com.kaifantech.init.sys.qualifier.UdfQualifier;
 import com.kaifantech.util.seq.ThreadID;
 
@@ -34,7 +34,7 @@ public class KfTestIotConnectAsClientTimer {
 
 	@Scheduled(cron = "0/1 * * * * ?")
 	public void resolute() {
-		if (UdfBusinessInfo.CURRENT_CLIENT.equals(BaseBusinessInfo.Clients.YUFENG)) {
+		if (UdfBusinessInfo.CURRENT_CLIENT.equals(AppBusinessInfo.Clients.YUFENG)) {
 			return;
 		}
 		if (!isRunning) {
--- ProjectBussKaifanTest/udf/com/kaifantech/init/sys/UdfBusinessInfo.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/init/sys/UdfBusinessInfo.java
@@ -1,6 +1,6 @@
 package com.kaifantech.init.sys;
 
-public class UdfBusinessInfo extends BaseBusinessInfo {
+public class UdfBusinessInfo extends AppBusinessInfo {
 	public static final Clients CURRENT_CLIENT = Clients.KF;
 	public static final Projects CURRENT_PROJECT = Projects.KF_TEST;
 
