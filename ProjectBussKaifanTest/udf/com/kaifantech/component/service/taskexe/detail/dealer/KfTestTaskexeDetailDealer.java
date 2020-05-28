29e8242ad1dfdb87bad25d747dfe9436de75a548
ahacs <583937325@qq.com>
2020-05-28 13:04:39 +0800

    Update KfTestTaskexeDetailDealer.java

--- ProjectBussKaifanTest/udf/com/kaifantech/component/service/taskexe/detail/dealer/KfTestTaskexeDetailDealer.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/service/taskexe/detail/dealer/KfTestTaskexeDetailDealer.java
@@ -16,22 +16,21 @@ import com.kaifantech.util.constant.taskexe.ArrivedActType;
 
 @Service
 public class KfTestTaskexeDetailDealer extends FancyTaskexeDetailDealer {
-	public boolean dealDetail(TaskexeBean taskexeBean, FancyAgvMsgBean fancyAgvMsgBean, TaskexeDetailBean taskexeDetail)
-			throws Exception {
-		if (ArrivedActType.WAIT.equals(taskexeDetail.getArrivedact())
-				|| ArrivedActType.LIFT_READY.equals(taskexeDetail.getArrivedact())) {
-			List<TaskexeDetailWorksBean> works = taskexeDetailWorksService.getWorks(taskexeDetail);
-			if (ArrivedActType.WAIT.equals(taskexeDetail.getArrivedact())) {
-				taskexeDetailWaitDealer.work(fancyAgvMsgBean, taskexeBean, taskexeDetail, works);
+	public boolean dealDetail(FancyAgvMsgBean agvMsg, TaskexeBean taskexe, TaskexeDetailBean detail) throws Exception {
+		if (ArrivedActType.WAIT.equals(detail.getArrivedact())
+				|| ArrivedActType.LIFT_READY.equals(detail.getArrivedact())) {
+			List<TaskexeDetailWorksBean> works = taskexeDetailWorksService.getWorks(detail);
+			if (ArrivedActType.WAIT.equals(detail.getArrivedact())) {
+				taskexeDetailWaitDealer.work(agvMsg, taskexe, detail, works);
 				return false;
-			} else if (ArrivedActType.LIFT_READY.equals(taskexeDetail.getArrivedact())) {
-				taskexeDetailWorksLiftDealer.work(fancyAgvMsgBean, taskexeBean, taskexeDetail, works);
+			} else if (ArrivedActType.LIFT_READY.equals(detail.getArrivedact())) {
+				taskexeDetailWorksLiftDealer.work(agvMsg, taskexe, detail, works);
 				return false;
 			}
 		}
 
-		AppSysParameters.setTaskstop(fancyAgvMsgBean.agvId(), false);
-		return super.dealDetail(taskexeBean, fancyAgvMsgBean, taskexeDetail);
+		AppSysParameters.setTaskstop(agvMsg.agvId(), false);
+		return super.dealDetail(agvMsg, taskexe, detail);
 	}
 
 	@Autowired
