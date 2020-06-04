cf09cd02da4b6ee6422f5d460c878ba4ef8012b4
ahacs <583937325@qq.com>
2020-06-04 15:28:33 +0800

    使用iacslockwatcher作为触发标准锁控制，档案局项目另行控制

--- ProjectBussKaifanTest/udf/com/kaifantech/component/service/taskexe/watch/KfTestLockWatcher.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/service/taskexe/watch/KfTestLockWatcher.java
@@ -1,17 +1,16 @@
 package com.kaifantech.component.service.taskexe.watch;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
+import com.kaifantech.bean.info.agv.AgvInfoBean;
+import com.kaifantech.bean.iot.client.IotClientBean;
 import com.kaifantech.bean.msg.fancy.agv.FancyAgvMsgBean;
-import com.kaifantech.bean.taskexe.TaskexeBean;
-import com.kaifantech.bean.taskexe.TaskexeDetailBean;
 import com.kaifantech.init.sys.qualifier.KfTestQualifier;
 
 @Service(KfTestQualifier.LOCK_WATCHER)
 public class KfTestLockWatcher extends AbsAcsLockWatcher<FancyAgvMsgBean> {
 	@Override
-	public void watch(FancyAgvMsgBean agvMsg, TaskexeBean taskexe, List<TaskexeDetailBean> details) throws Exception {
+	public void watch(IotClientBean iotClientBean, AgvInfoBean agvInfo) throws Exception {
+
 	}
 }
