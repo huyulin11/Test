36af566c654056101e6e94973d31dd3168050231
ahacs <583937325@qq.com>
2020-06-12 14:08:10 +0800

    Update KfTestRestController.java

--- ProjectBussKaifanTest/udf/com/kaifantech/component/controller/de/KfTestRestController.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/controller/de/KfTestRestController.java
@@ -18,18 +18,14 @@ import com.kaifantech.component.dao.agv.info.AgvOpDeliverDao;
 import com.kaifantech.component.dao.agv.info.AgvOpFetchDao;
 import com.kaifantech.component.dao.agv.info.AgvOpInitDao;
 import com.kaifantech.component.dao.agv.info.AgvOpTransportDao;
-import com.kaifantech.component.log.AgvStatusDBLogger;
 import com.kaifantech.component.service.iot.client.IIotClientService;
 import com.kaifantech.component.service.status.agv.AgvStatusService;
 import com.kaifantech.component.service.taskexe.add.IFancyTaskexeAddService;
 import com.kaifantech.component.service.taskexe.info.TaskexeInfoService;
-import com.kaifantech.init.sys.params.AppAutoParameters;
-import com.kaifantech.init.sys.params.AppSysParameters;
 import com.kaifantech.init.sys.qualifier.UdfQualifier;
-import com.kaifantech.util.constant.taskexe.ctrl.AgvCtrlType;
+import com.kaifantech.util.constant.taskexe.ctrl.AgvCmdCtrlTask;
 import com.kaifantech.util.constant.taskexe.ctrl.AgvMoveStatus;
 import com.kaifantech.util.constant.taskexe.ctrl.AgvTaskType;
-import com.kaifantech.util.log.AppFileLogger;
 import com.kaifantech.util.thread.ThreadTool;
 
 @Controller
@@ -91,38 +87,9 @@ public class KfTestRestController {
 	@ResponseBody
 	public JSONObject addCtrlTask(String tasktype, Integer agvId) {
 		try {
-			if (AgvCtrlType.AGVS_STOP_PI.equals(tasktype) || AgvCtrlType.AGVS_OPEN_PI.equals(tasktype)) {
-				boolean num = AppSysParameters.setOpenPi(AgvCtrlType.AGVS_OPEN_PI.equals(tasktype));
-				String msg = (AgvCtrlType.AGVS_STOP_PI.equals(tasktype) ? AgvCtrlType.AGVS_STOP_PI_STR
-						: AgvCtrlType.AGVS_OPEN_PI_STR) + num;
-				AppFileLogger.setWarningTips(agvId, msg, 0, AgvStatusDBLogger.MSG_LEVEL_WARNING);
-				return new AppMsg(0, msg + ":更新成功！").toAlbbJson();
-			}
-
-			if (AgvCtrlType.AGVS_STOP_AUTO_TASK.equals(tasktype) || AgvCtrlType.AGVS_OPEN_AUTO_TASK.equals(tasktype)) {
-				boolean num = AppAutoParameters.setAutoTask(!AgvCtrlType.AGVS_STOP_AUTO_TASK.equals(tasktype));
-				String msg = (AgvCtrlType.AGVS_STOP_AUTO_TASK.equals(tasktype) ? AgvCtrlType.AGVS_STOP_AUTO_TASK_STR
-						: AgvCtrlType.AGVS_OPEN_AUTO_TASK_STR) + num;
-				AppFileLogger.setWarningTips(agvId, msg, 0, AgvStatusDBLogger.MSG_LEVEL_WARNING);
-				return new AppMsg(0, msg + ":更新成功！").toAlbbJson();
-			}
-
-			if (AgvCtrlType.AGVS_STOP_AUTO_CHARGE.equals(tasktype)
-					|| AgvCtrlType.AGVS_OPEN_AUTO_CHARGE.equals(tasktype)) {
-				boolean num = AppAutoParameters.setAutoCharge(!AgvCtrlType.AGVS_STOP_AUTO_CHARGE.equals(tasktype));
-				String msg = (AgvCtrlType.AGVS_STOP_AUTO_CHARGE.equals(tasktype) ? AgvCtrlType.AGVS_STOP_AUTO_CHARGE_STR
-						: AgvCtrlType.AGVS_OPEN_AUTO_CHARGE_STR) + num;
-				AppFileLogger.setWarningTips(agvId, msg, 0, AgvStatusDBLogger.MSG_LEVEL_WARNING);
-				return new AppMsg(0, msg + ":更新成功！").toAlbbJson();
-			}
-
-			if (AgvCtrlType.AGVS_STOP_ERR_BACK.equals(tasktype) || AgvCtrlType.AGVS_OPEN_ERR_BACK.equals(tasktype)) {
-				boolean num = AppAutoParameters.setErrBack(!AgvCtrlType.AGVS_STOP_ERR_BACK.equals(tasktype));
-				String msg = (AgvCtrlType.AGVS_STOP_ERR_BACK.equals(tasktype) ? AgvCtrlType.AGVS_STOP_ERR_BACK_STR
-						: AgvCtrlType.AGVS_OPEN_ERR_BACK_STR) + num;
-				AppFileLogger.setWarningTips(agvId, msg, 0, AgvStatusDBLogger.MSG_LEVEL_WARNING);
-				return new AppMsg(0, msg + ":更新成功！").toAlbbJson();
-			}
+			AppMsg appMsg = AgvCmdCtrlTask.addCtrlTask(tasktype, agvId);
+			if (appMsg.isSuccess())
+				return appMsg.toAlbbJson();
 
 			if (AgvTaskType.GOTO_CHARGE.equals(tasktype)) {
 				agvOpChargeDao.commandToCharge(agvId);
