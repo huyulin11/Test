477924074df1f99774066b0d6b07b0ee5afcbc8e
ahacs <583937325@qq.com>
2020-06-23 17:06:38 +0800

    Update KfTestRestController.java

--- ProjectBussKaifanTest/udf/com/kaifantech/component/controller/de/KfTestRestController.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/controller/de/KfTestRestController.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
-import com.calculatedfun.util.AppTool;
 import com.calculatedfun.util.msg.AppMsg;
-import com.kaifantech.bean.iot.client.IotClientBean;
 import com.kaifantech.bean.taskexe.AgvStatusBean;
 import com.kaifantech.bean.taskexe.TaskexeBean;
-import com.kaifantech.cache.manager.AppCache;
-import com.kaifantech.component.dao.agv.info.AgvOpChargeDao;
 import com.kaifantech.component.dao.agv.info.AgvOpDeliverDao;
 import com.kaifantech.component.dao.agv.info.AgvOpFetchDao;
 import com.kaifantech.component.dao.agv.info.AgvOpInitDao;
@@ -21,12 +17,8 @@ import com.kaifantech.component.dao.agv.info.AgvOpTransportDao;
 import com.kaifantech.component.service.iot.client.IIotClientService;
 import com.kaifantech.component.service.status.agv.AgvStatusService;
 import com.kaifantech.component.service.taskexe.add.IFancyTaskexeAddService;
-import com.kaifantech.component.service.taskexe.info.TaskexeInfoService;
 import com.kaifantech.init.sys.qualifier.UdfQualifier;
-import com.kaifantech.util.constant.taskexe.ctrl.AgvCmdCtrlTask;
-import com.kaifantech.util.constant.taskexe.ctrl.AgvMoveStatus;
 import com.kaifantech.util.constant.taskexe.ctrl.AgvTaskType;
-import com.kaifantech.util.thread.ThreadTool;
 
 @Controller
 @RequestMapping("/de/acs/")
@@ -41,12 +33,6 @@ public class KfTestRestController {
 	@Autowired
 	@Qualifier(UdfQualifier.DEFAULT_IOT_CLIENT_SERVICE)
 	private IIotClientService iotClientService;
-
-	@Autowired
-	private TaskexeInfoService taskexeInfoService;
-
-	@Autowired
-	private AgvOpChargeDao agvOpChargeDao;
 
 	@Autowired
 	private AgvOpTransportDao agvOpTransportDao;
@@ -76,71 +62,6 @@ public class KfTestRestController {
 				return new AppMsg(-1, "无法识别的任务类型:" + tasktype).toAlbbJson();
 			}
 			AppMsg msg = agvStatusService.addStatus(new AgvStatusBean(tasktype, agvId, 0));
-			return msg.toAlbbJson();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new AppMsg(-1, "发生错误" + e.getMessage()).toAlbbJson();
-		}
-	}
-
-	@RequestMapping("addCtrlTask2")
-	@ResponseBody
-	public JSONObject addCtrlTask(String tasktype, Integer agvId) {
-		try {
-			AppMsg appMsg = AgvCmdCtrlTask.addCtrlTask(tasktype, agvId);
-			if (appMsg.isSuccess())
-				return appMsg.toAlbbJson();
-
-			if (AgvTaskType.GOTO_CHARGE.equals(tasktype)) {
-				agvOpChargeDao.commandToCharge(agvId);
-			} else if (AgvTaskType.BACK_CHARGE.equals(tasktype)) {
-				agvOpChargeDao.commandBackCharge(agvId);
-			} else if (AgvMoveStatus.PAUSE_USER.equals(tasktype)) {
-				agvOpChargeDao.pauseByUser(agvId);
-				if (agvId.equals(0)) {
-					for (IotClientBean agv : iotClientService.getAgvCacheList()) {
-						agvOpChargeDao.pauseByUser(agv.getId());
-					}
-				}
-			} else if (AgvMoveStatus.CONTINUE.equals(tasktype)) {
-				agvOpChargeDao.startup(agvId);
-			} else if (AgvMoveStatus.SHUTDOWN.equals(tasktype)) {
-				TaskexeBean taskexeBean = taskexeInfoService.getNextOne(agvId);
-				if (!AppTool.isNull(taskexeBean)) {
-					taskexeBean.setShutdown();
-				} else {
-					agvOpInitDao.workOverForce(agvId);
-				}
-			} else if (AgvTaskType.CONFIRM.equals(tasktype)) {
-				AppCache.worker().hset("CONFIRM", agvId, true);
-			} else if (AgvTaskType.GOTO_INIT.equals(tasktype)) {
-				agvOpInitDao.commandToInit(agvId);
-			} else if (AgvTaskType.RE_PATH.equals(tasktype)) {
-				TaskexeBean taskexeBean = taskexeInfoService.getNextOne(agvId);
-				if (!AppTool.isNull(taskexeBean)) {
-					ThreadTool.run(() -> {
-						try {
-							taskexeAddService.rePath(taskexeBean);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}, true);
-				}
-			}
-
-			AppMsg msg = agvStatusService.addStatus(new AgvStatusBean(tasktype, agvId, 0));
-			return msg.toAlbbJson();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new AppMsg(-1, "发生错误" + e.getMessage()).toAlbbJson();
-		}
-	}
-
-	@RequestMapping("addTask2")
-	@ResponseBody
-	public JSONObject addTask(String tasktype, Integer agvId) {
-		try {
-			AppMsg msg = taskexeAddService.addTask(new TaskexeBean(tasktype, agvId, 0));
 			return msg.toAlbbJson();
 		} catch (Exception e) {
 			e.printStackTrace();
