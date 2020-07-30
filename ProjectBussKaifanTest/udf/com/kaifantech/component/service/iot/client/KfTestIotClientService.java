b97e128b07fd96f5288c0b986d835424ae621738
ahacs <583937325@qq.com>
2020-07-30 10:18:26 +0800

    Update KfTestIotClientService.java

--- ProjectBussKaifanTest/udf/com/kaifantech/component/service/iot/client/KfTestIotClientService.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/service/iot/client/KfTestIotClientService.java
@@ -1,25 +1,18 @@
 package com.kaifantech.component.service.iot.client;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.calculatedfun.util.AppTool;
 import com.kaifantech.bean.iot.client.IotClientBean;
 import com.kaifantech.init.sys.qualifier.KfTestQualifier;
 import com.kaifantech.util.constant.taskexe.ctrl.IotDevType;
-import com.calculatedfun.util.AppTool;
 
 @Service(KfTestQualifier.IOT_CLIENT_SERVICE)
 public class KfTestIotClientService extends AcsIotClientService {
 	private List<IotClientBean> agvCacheList = new ArrayList<>();
-
-	@Scheduled(cron = "0 0 0/1 * * ?")
-	public void shuffleAgvList() {
-		Collections.shuffle(getAgvCacheList());
-	}
 
 	public synchronized List<IotClientBean> getAgvCacheList() {
 		if (AppTool.isNull(agvCacheList)) {
