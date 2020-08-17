794b555d1c9840b91dc4722670aded705a2cd4db
ahacs <583937325@qq.com>
2020-08-17 18:10:25 +0800

    Update KfTestLockWatcher.java

--- ProjectBussKaifanTest/udf/com/kaifantech/component/service/taskexe/watch/KfTestLockWatcher.java
+++ ProjectBussKaifanTest/udf/com/kaifantech/component/service/taskexe/watch/KfTestLockWatcher.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Service;
 
 import com.kaifantech.bean.info.agv.AgvInfoBean;
 import com.kaifantech.bean.iot.client.IotClientBean;
-import com.kaifantech.bean.msg.agv.FancyAgvMsgBean;
 import com.kaifantech.init.sys.qualifier.KfTestQualifier;
 
 @Service(KfTestQualifier.LOCK_WATCHER)
-public class KfTestLockWatcher extends AbsAcsLockWatcher<FancyAgvMsgBean> {
+public class KfTestLockWatcher extends AbsFancyLockWatcher {
 	@Override
 	public void watch(IotClientBean iotClientBean, AgvInfoBean agvInfo) throws Exception {
 
